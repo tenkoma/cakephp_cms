32297a13f1bc75296ae0d0291185b7dda23eebf8
Koji Tanaka <tenkoma@gmail.com>
2017-11-13 00:14:52 +0900

    article add

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -3,11 +3,27 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Utility\Text;
 
 class ArticlesTable extends Table
 {
     public function initialize(array $config)
     {
         $this->addBehavior('Timestamp');
+    }
+
+    public function beforeSave($event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            // スラグをスキーマで定義されている最大長に調整
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
+
+        // これは一時的なもので、後で認証を構築するときに
+        // 削除されます。
+        if (!$entity->user_id) {
+            $entity->user_id = 1;
+        }
     }
 }
