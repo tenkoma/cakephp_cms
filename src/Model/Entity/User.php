7e08b9b7a5ba1e06685fe09d9fe4f97e200c79cb
Koji Tanaka <tenkoma@gmail.com>
2017-11-18 21:08:41 +0900

    パスワードのハッシュ化を実装

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Model\Entity;
 
+use Cake\Auth\DefaultPasswordHasher;
 use Cake\ORM\Entity;
 
 /**
@@ -42,4 +43,13 @@ class User extends Entity
     protected $_hidden = [
         'password'
     ];
+
+    protected function _setPassword($value)
+    {
+        if (strlen($value)) {
+            $hasher = new DefaultPasswordHasher();
+
+            return $hasher->hash($value);
+        }
+    }
 }
